bb41325654b863806f21a3d77d424fb0909d6909
kanashiima <89482908+kanashiima@users.noreply.github.com>
2021-11-01 20:15:51 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+
+// i dont know for sure i dont have enough time to make this work right but i was gonna have it auto play on page in window when clicked just to use some js but w/e 
+
+
+
+// function play1(){
+//     var player = <iframe width="560" height="315" 
+//     src="https://www.youtube.com/watch?v=GD_rb6ATsJE&list=LL&index=46" frameborder="0" 
+//     allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in- 
+//     picture" allowfullscreen></iframe>;
+//     document.getElementById('ID').innerHtml = player;
+//   }
